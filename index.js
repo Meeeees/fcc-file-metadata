85c298b4fc03053750d24cb60d124b508947dd41
meeeeesgithub <119116083+meeeeesgithub@users.noreply.github.com>
2023-07-18 22:14:28 +0200

    final project

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require('express');
+// enable express file upload
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
 var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(fileUpload());
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +18,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', function (req, res) {
+  if (!req.files) {
+    return res.status(400).send('No files were uploaded.');
+  }
+const { upfile }  = req.files
+  res.json({ name: upfile.name, type: upfile.mimetype, size: upfile.size })
+})
 
 
 
